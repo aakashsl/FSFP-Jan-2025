3ef5a6a0b973d3b2b0a0f27a32a9b6d545876523
Aakash SL <slaakash06@gmail.com>
2025-02-10 19:46:28 +0530

    Day 15

---
+++ FSFP JAN 25/Day 15/main.js
@@ -0,0 +1,30 @@
+console.log("External Working")
+
+// 1. without Parameter, without Return
+// 2. with parameter , without Return
+// 3. without parameter, with Return
+// 4. with parameter, with Return
+
+// function display(){
+//    console.log("Display Function");
+// }
+
+// function add(a,b) {
+//    console.log(a+b)
+// }
+
+// function dis2() {
+//    return "Hello";
+// }
+
+// c=dis2()
+
+// console.log(c)
+
+// function add2(a,b) {
+//    return a+b
+// }
+
+// d=add2(5,6)
+// console.log(d)
+
